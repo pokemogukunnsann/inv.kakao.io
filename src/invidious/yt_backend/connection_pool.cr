39bd4160969a4c015098e9bd7489c6b74267c0c6
pokemogukunnsann <konnnitiha.sayounara@icloud.com>
2025-10-30 07:26:16 +0900

    Update connection_pool.cr

--- src/invidious/yt_backend/connection_pool.cr
+++ src/invidious/yt_backend/connection_pool.cr
@@ -1,3 +1,5 @@
+require "../../http_pool" # 💡 DB::PoolからHttpPoolに変更
+
 # Mapping of subdomain => YoutubeConnectionPool
 # This is needed as we may need to access arbitrary subdomains of ytimg
 private YTIMG_POOLS = {} of String => YoutubeConnectionPool
@@ -6,7 +8,7 @@ struct YoutubeConnectionPool
   property! url : URI
   property! capacity : Int32
   property! timeout : Float64
-  property pool : DB::Pool(HTTP::Client)
+  property pool : HttpPool(HTTP::Client) # 💡 DB::PoolからHttpPoolに変更
 
   def initialize(url : URI, @capacity = 5, @timeout = 5.0)
     @url = url
@@ -33,31 +35,31 @@ struct YoutubeConnectionPool
   end
 
   private def build_pool
-    options = DB::Pool::Options.new(
+    options = HttpPool::Options.new( # 💡 DB::Pool::OptionsからHttpPool::Optionsに変更
       initial_pool_size: 0,
       max_pool_size: capacity,
       max_idle_pool_size: capacity,
       checkout_timeout: timeout
     )
 
-    DB::Pool(HTTP::Client).new(options) do
+    HttpPool(HTTP::Client).new(options) do # 💡 DB::PoolからHttpPoolに変更
       next make_client(url, force_resolve: true)
     end
   end
 end
 
 struct CompanionConnectionPool
-  property pool : DB::Pool(HTTP::Client)
+  property pool : HttpPool(HTTP::Client) # 💡 DB::PoolからHttpPoolに変更
 
   def initialize(capacity = 5, timeout = 5.0)
-    options = DB::Pool::Options.new(
+    options = HttpPool::Options.new( # 💡 DB::Pool::OptionsからHttpPool::Optionsに変更
       initial_pool_size: 0,
       max_pool_size: capacity,
       max_idle_pool_size: capacity,
       checkout_timeout: timeout
     )
 
-    @pool = DB::Pool(HTTP::Client).new(options) do
+    @pool = HttpPool(HTTP::Client).new(options) do # 💡 DB::PoolからHttpPoolに変更
       companion = CONFIG.invidious_companion.sample
       next make_client(companion.private_url, use_http_proxy: false)
     end
