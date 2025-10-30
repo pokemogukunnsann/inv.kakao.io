a0912f173e747e8e32b5be58381b8c9df33f4208
pokemogukunnsann <konnnitiha.sayounara@icloud.com>
2025-10-30 12:27:28 +0900

    Create http_ext.cr

---
+++ src/invidious/helpers/http_ext.cr
@@ -0,0 +1,28 @@
+require "http/cookie" # HTTP::Cookie::parse を使うために必要
+
+# HTTP::Cookies クラスを拡張します
+class HTTP::Cookies
+  # 文字列を受け取ってクッキーコレクションを生成する new メソッドを定義
+  # YAMLコンバータはこれを利用し、クッキー文字列を解析します。
+  def initialize(@cookies = [] of HTTP::Cookie, str : String)
+    # 文字列が空でなければ解析する
+    unless str.empty?
+      str.split(';').each do |cookie_string|
+        cookie_string = cookie_string.strip
+        if cookie_string.includes?('=')
+          # 単一のクッキー文字列として解析
+          if cookie = HTTP::Cookie.parse(cookie_string)
+            @cookies << cookie
+          end
+        end
+      end
+    end
+  end
+
+  # 引数なしの既存の new メソッドもオーバーロードで保持
+  def initialize(@cookies = [] of HTTP::Cookie)
+  end
+end
+
+puts "HTTPExt: HTTP::Cookies.new(string) ヘルパーメソッドを作成しました。"
+# HTTPExt: HTTP::Cookies.new(string) ヘルパーメソッドを作成しました。
