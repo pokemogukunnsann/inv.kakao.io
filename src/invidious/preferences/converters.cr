2980908b5f4b78d0f7809078e32fed8d0dcb2c02
pokemogukunnsann <konnnitiha.sayounara@icloud.com>
2025-10-30 13:05:54 +0900

    Update converters.cr

--- src/invidious/preferences/converters.cr
+++ src/invidious/preferences/converters.cr
@@ -3,7 +3,7 @@ require "uri" # URIConverterで必要
 require "socket" # FamilyConverterで必要
 require "http/cookie" # StringToCookiesで必要
 require "yaml"
-require "./preferences/*"
+require "./*"
 require "../../helpers/http_ext" # または他の必要なファイル
 
 # Preferenceはclassであるため、classで拡張します。
